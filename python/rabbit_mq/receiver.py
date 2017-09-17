8fa69f93f1cc1f44e42abd509fc8bf05d8c41831
MrTurtleW <yqgffm@gmail.com>
2017-09-17 11:54:28 +0800

    2017-09-17

--- python/rabbit_mq/receiver.py
+++ python/rabbit_mq/receiver.py
@@ -9,11 +9,17 @@ channel = connection.channel()
 # create a queue named hello.
 channel.queue_declare(queue='hello')
 
+# fair dispatch
+channel.basic_qos(prefetch_count=1)  
+
 # define a callback function to process the data received
 # %r using the repr() method to process object
 # %s using the str() method to process object
 def callback(ch, method, properties, body):
     print 'Received %r' % (body)
+
+    # ack
+    # ch.basic_ack(delivery_tag = method.delivery_tag) 
 
 channel.basic_consume(callback,
     queue='hello',
